83e9f22f516b447126dfcf919c06f88bf521e270
Djavan Cackley <djavancackley@Djavans-MBP.home>
2019-06-01 11:11:29 -1000

    successful aws dev to master merge now going to push shan branch to dev on git hub

--- amplify/backend/function/testapi/src/app.js
+++ amplify/backend/function/testapi/src/app.js
@@ -6,6 +6,8 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
+//FUCK YEAH OUR MERGE WERKED//
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
@@ -16,7 +18,7 @@ app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
@@ -27,60 +29,60 @@ app.use(function(req, res, next) {
  * Example get method *
  **********************/
 
-app.get('/people', function(req, res) {
+app.get('/people', function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  res.json({ success: 'get call succeed!', url: req.url });
 });
 
-app.get('/people/*', function(req, res) {
+app.get('/people/*', function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
-
-/****************************
-* Example post method *
-****************************/
-
-app.post('/people', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
-
-app.post('/people/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  res.json({ success: 'get call succeed!', url: req.url });
 });
 
 /****************************
 * Example post method *
 ****************************/
 
-app.put('/people', function(req, res) {
+app.post('/people', function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({ success: 'post call succeed!', url: req.url, body: req.body })
 });
 
-app.put('/people/*', function(req, res) {
+app.post('/people/*', function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({ success: 'post call succeed!', url: req.url, body: req.body })
+});
+
+/****************************
+* Example post method *
+****************************/
+
+app.put('/people', function (req, res) {
+  // Add your code here
+  res.json({ success: 'put call succeed!', url: req.url, body: req.body })
+});
+
+app.put('/people/*', function (req, res) {
+  // Add your code here
+  res.json({ success: 'put call succeed!', url: req.url, body: req.body })
 });
 
 /****************************
 * Example delete method *
 ****************************/
 
-app.delete('/people', function(req, res) {
+app.delete('/people', function (req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  res.json({ success: 'delete call succeed!', url: req.url });
 });
 
-app.delete('/people/*', function(req, res) {
+app.delete('/people/*', function (req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  res.json({ success: 'delete call succeed!', url: req.url });
 });
 
-app.listen(3000, function() {
-    console.log("App started")
+app.listen(3000, function () {
+  console.log("App started")
 });
 
 // Export the app object. When executing the application local this does nothing. However,
